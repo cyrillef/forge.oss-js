a783e4929428ec2eb0235837e2cc71a4acc144c2
cyrillef <cyrillef@free.fr>
2017-02-09 13:24:49 +0100

    deprecate module and use forge-apis

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@
  * Do not edit the class manually.
  *
  */
-console.warn( "WARNING: The 'forge.oss-js' npm package is deprecated in favor of the 'forge-apis' npm package, please upgrade.");
+console.warn( "WARNING: The 'forge-oss' npm package is deprecated in favor of the 'forge-apis' npm package, please upgrade.");
 
 (function(factory) {
   if (typeof define === 'function' && define.amd) {
